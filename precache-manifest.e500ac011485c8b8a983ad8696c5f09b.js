3d3d30183fa28f9e9dce8828cd87e0eaadead412
badimihai-dev <mihai.badila.dev@gmail.com>
2020-04-05 19:49:36 +0300

    Updates

---
+++ precache-manifest.e500ac011485c8b8a983ad8696c5f09b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59101031f23bd3c62314eb00205441fa",
+    "url": "/chat-app/index.html"
+  },
+  {
+    "revision": "08d16831ecd73a63d0d6",
+    "url": "/chat-app/static/css/main.a25cdf74.chunk.css"
+  },
+  {
+    "revision": "96ba92e5a7dbf3cb4519",
+    "url": "/chat-app/static/js/2.042ccff8.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/chat-app/static/js/2.042ccff8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08d16831ecd73a63d0d6",
+    "url": "/chat-app/static/js/main.da38d2aa.chunk.js"
+  },
+  {
+    "revision": "eb876776f94849671ee4",
+    "url": "/chat-app/static/js/runtime-main.dab4f9e9.js"
+  }
+]);
